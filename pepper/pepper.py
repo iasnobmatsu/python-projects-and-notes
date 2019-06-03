b91c5962e2d02d47bd94846fc5cc3e53732c0e9a
Clara Xu <xuclara98@gmail.com>
2019-06-03 15:33:44 +0800

    pepper2

--- pepper/pepper.py
+++ pepper/pepper.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app=Flask(__name__)
 
+app.config['SECRET_KEY']="9a62337156cabd15a1744c7a1706c4a1"
 
 #export FLASK_APP=blog.py
 #flask run
 #export FLASK_DEBUG=1--debug mode update without exit
+
+
+
+#pip3 install flask_wtf
+
 
 
 #dummy data
@@ -24,13 +31,35 @@ posts=[
 
 
 @app.route("/")
+@app.route("/homw")
 def home():
     return render_template('home.html',posts=posts, title="Pepper: Blog, Share, More")
 
 
 @app.route("/explore")
 def explore():
-    return render_template('explore.html',posts=posts)
+    return render_template('explore.html', title="Pepper:Explore", posts=posts)
+
+
+@app.route("/signup", methods=['GET','POST'])
+def signup():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash("Signed up successfully for {}".format(form.username.data),"success")
+        return redirect(url_for('home'))
+    return render_template("signup.html", title="Pepper: Sign Up", form=form)
+
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        if form.email.data=="admin@pepper.com" and form.password.data=="pepper":
+            flash("Login successfully for {}".format(form.email.data),"success")
+            return redirect(url_for('home'))
+        else:
+            flash("Login Unsuccessful for {}".format(form.email.data),"danger")
+    return render_template("login.html", title="Pepper: Login", form=form)
 
 # if __name__=="__main__":
 #     app.run(debug=True)
