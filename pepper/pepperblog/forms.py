d26defcb2a421f6ff750e4801e8fd0ffffb65c0e
Clara Xu <xuclara98@gmail.com>
2019-06-05 09:34:41 +0800

    pepper3

--- pepper/pepperblog/forms.py
+++ pepper/pepperblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
+from pepperblog.models import User
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 class RegistrationForm(FlaskForm):
     username=StringField("Username", validators=[DataRequired(), Length(min=5,max=20)])
@@ -8,6 +9,16 @@ class RegistrationForm(FlaskForm):
     password= PasswordField("Password", validators=[DataRequired(), Length(min=5,max=20)])
     confirm=PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
     submit=SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user=User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username entered already exist.")
+            
+    def validate_email(self, email):
+        email=User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError("Email entered already used.")
     
 class LoginForm(FlaskForm):
     email=StringField("Email",validators=[DataRequired(),Email()])
--- pepper/pepperblog/routes.py
+++ pepper/pepperblog/routes.py
@@ -1,18 +1,12 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-app=Flask(__name__)
-
-app.config['SECRET_KEY']="9a62337156cabd15a1744c7a1706c4a1"
+from flask import render_template, url_for, flash, redirect, request
+from pepperblog import app, db, bcrypt
+from pepperblog.forms import RegistrationForm, LoginForm
+from pepperblog.models import User, Post
+from flask_login import login_user, logout_user,current_user, login_required
 
 #export FLASK_APP=blog.py
 #flask run
 #export FLASK_DEBUG=1--debug mode update without exit
-
-
-
-#pip3 install flask_wtf
-
-
 
 #dummy data
 posts=[
@@ -31,7 +25,7 @@ posts=[
 
 
 @app.route("/")
-@app.route("/homw")
+@app.route("/home")
 def home():
     return render_template('home.html',posts=posts, title="Pepper: Blog, Share, More")
 
@@ -43,23 +37,48 @@ def explore():
 
 @app.route("/signup", methods=['GET','POST'])
 def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form=RegistrationForm()
     if form.validate_on_submit():
-        flash("Signed up successfully for {}".format(form.username.data),"success")
-        return redirect(url_for('home'))
+        hashed_pw=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user=User(username=form.username.data,email=form.email.data,password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created for {}. You can login now.".format(form.username.data),"success")
+        return redirect(url_for('login'))
     return render_template("signup.html", title="Pepper: Sign Up", form=form)
 
 
 @app.route("/login",methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form=LoginForm()
     if form.validate_on_submit():
-        if form.email.data=="admin@pepper.com" and form.password.data=="pepper":
-            flash("Login successfully for {}".format(form.email.data),"success")
-            return redirect(url_for('home'))
+        user=User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            login_user(user,remember=form.remember.data) #testpw=username.lower()
+            page=request.args.get('next')
+            if page:
+                return redirect(page)
+            else:
+                return redirect(url_for('home'))
+
         else:
             flash("Login Unsuccessful for {}".format(form.email.data),"danger")
     return render_template("login.html", title="Pepper: Login", form=form)
 
-# if __name__=="__main__":
-#     app.run(debug=True)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route("/account")
+@login_required
+def account():
+    # if current_user.is_authenticated:
+    return render_template("account.html", title="Pepper: Account")
+    # else:
+    #     return redirect(url_for('login'))
